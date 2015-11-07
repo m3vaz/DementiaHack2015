1d3b7157fc773258fe17410dbc5ab208dfe1057e
Joyce Zhang <joycezhang14@gmail.com>
2015-11-07 16:50:21 -0500

    Updated website js

--- DementiaHackWebsite/startbootstrap-business-casual-1.0.3/js/demo.js
+++ DementiaHackWebsite/startbootstrap-business-casual-1.0.3/js/demo.js
@@ -7,30 +7,58 @@ $(document).ready(function(){
         factor = 120;
         
         //beacon 1:
-        $("canvas").drawArc({
+        $("canvas").drawPolygon({
             fillStyle: 'black',
+            sides:3,
             x : numBorder, y : numBorder, //(0,0)
             radius: 10
         })
         
         //beacon 2: 
-        .drawArc({
+        .drawPolygon({
             fillStyle: 'black',
+            sides: 3,
             x : numBorder , y : numBorder + 5*factor, //(0,5) distances are multiplied by 5 and add 20
             radius: 10
         })
         
         //beacon 3: 
-        .drawArc({
+        .drawPolygon({
             fillStyle: 'black',
+            sides: 3,
             x : numBorder + 5*factor , y : numBorder, //(0,5)
             radius: 10
         });
         
-        //get the data and then plot it 
-        while(){
+        //Tracker:
             
+        //Get data and plot it every second
+        var tid = setInterval(track,1000);
+        
+        //get position data
+        function getData(){
+            $.ajax({
+                url:"http://mensangeli.mybluemix.net/",
+                type: "GET",
+                success : track
+            }) 
         }
-    });
+        
+        function track(data){
+            debugger;
+            $("canvas").text(data);
+            /*        
+            //plot position data
+            xData = ;
+            yData = ;
+            
+            .drawArc({
+                fillStyle: 'red',
+                x : numBorder + xData* factor , y : numBorder + yData* factor, 
+                radius: 10
+            });
+            */
+        }
+    }); 
 });
 
