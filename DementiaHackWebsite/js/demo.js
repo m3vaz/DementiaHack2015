e9e418a42c33e72dfc87be7b2e2ff377d9616067
Joyce Zhang <joycezhang14@gmail.com>
2015-11-07 20:14:50 -0500

    Working Red dot

--- DementiaHackWebsite/js/demo.js
+++ DementiaHackWebsite/js/demo.js
@@ -1,6 +1,32 @@
+//Data format: {"time": "2015-11-07 15:33:36.714000", "y": 2.883, "uuid": "0x0000", "x": 3.424}
 $(document).ready(function(){
+    //get position data
+    function track(data){  
+        //process the data string
+        dataObject = JSON.parse(data);
+        //get time data for checking purposes
+        time = dataObject.time;
+        //plot position data
+        xData = dataObject.x;
+        yData = dataObject.y;
+        debugger;
+        
+        $("canvas").drawArc({
+            fillStyle: 'red',
+            x : numBorder + xData* factor , y : numBorder + yData* factor, 
+            radius: 10
+        });
+    }
+    
+    function getData(){
+        debugger;
+        url = 'http://mensangeli.mybluemix.net/datapull/';
+        data = {uuid:'0x0000'};
+        $.post(url, data).done(track)
+    }
     // when the button is clicked, start demo!
     $(".button").click(function(){
+        debugger;
         //draw the ibeacons on the canvas
         //modifies the position of the beacons to better fit the screen
         numBorder = 30;
@@ -31,43 +57,10 @@ $(document).ready(function(){
         });
         
         //Tracker:
-            
+        //getData();
         //Get data and plot it every second
-        //var tid = setInterval(getData,1000);
-        
-        //get position data
-        function getData(){
-            $.ajax({
-                url:"http://mensangeli.mybluemix.net/",
-                type: "GET",
-                dataType:'jsonp',
-                success : track,
-            }) 
-        }
-        
-        function track(data){
-            debugger;
-            $("canvas").drawText({
-              fillStyle: '#9cf',
-              strokeStyle: '#25a',
-              strokeWidth: 2,
-              x: 150, y: 100,
-              fontSize: 48,
-              fontFamily: 'Verdana, sans-serif',
-              text: data,
-            });
-            /*        
-            //plot position data
-            xData = ;
-            yData = ;
-            
-            .drawArc({
-                fillStyle: 'red',
-                x : numBorder + xData* factor , y : numBorder + yData* factor, 
-                radius: 10
-            });
-            */
-        }
+        var tid = setInterval(getData,10000);
+       
     }); 
 });
 
