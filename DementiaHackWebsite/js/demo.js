2cc8e24190f14ea437d1268ea57fb73d62d58be7
Joyce Zhang <joycezhang14@gmail.com>
2015-11-08 00:49:27 -0500

    Updates to javascript

--- DementiaHackWebsite/js/demo.js
+++ DementiaHackWebsite/js/demo.js
@@ -1,43 +1,81 @@
 //Data format: {"time": "2015-11-07 15:33:36.714000", "y": 2.883, "uuid": "0x0000", "x": 3.424}
 $(document).ready(function(){
+    
+    numBorder = 30;
+    factor = 120;
+    radius = 20;
+    k = 0;
+    
+    //constructor for tracker 
+    function trackShape(color,xData, yData,time){
+        this.color = color;
+        x = xData;
+        y = yData; 
+        this.time = time;
+    }
+    
+    trackObject = new trackShape('#00E500',0,0,0);
+    
     //get position data
     function track(data){  
+        k = 0;
+        
         //process the data string
         dataObject = JSON.parse(data);
+        
         //get time data for checking purposes
-        time = dataObject.time;
+        trackObject.time = dataObject.time;
+        
         //plot position data
-        xData = dataObject.x;
-        yData = dataObject.y;
-        debugger;
+        trackObject.x = numBorder + dataObject.x* factor;
+        trackObject.y = numBorder + dataObject.y* factor; 
+        //debugger;
         
         $("canvas").drawArc({
-            fillStyle: 'red',
-            x : numBorder + xData* factor , y : numBorder + yData* factor, 
-            radius: 10
+            fillStyle: trackObject.color,
+            x : trackObject.x, y : trackObject.y, 
+            strokeStyle:'black',
+            radius: radius,
         });
+    }
+    //Act if there was no data found
+    function noData(){
+        debugger;
+        k = k+1;
+        currentTime = new Date($.now());
+        
+        if((k >= 5) && ((currentTime - trackObject.time) >= 5000)){
+            debugger;
+            trackObject.color = 'red',            
+            $("canvas").drawArc({
+                fillStyle: trackObject.color,
+                x : trackObject.x, y : trackObject.y, 
+                strokeStyle:'black',
+                radius: radius,
+            });      
+        }
     }
     
     function getData(){
-        debugger;
+        //debugger;
         url = 'http://mensangeli.mybluemix.net/datapull/';
         data = {uuid:'0x0000'};
+        
         $.post(url, data).done(track)
+        $.post(url, data).fail(noData)
     }
+    
+    
     // when the button is clicked, start demo!
     $(".button").click(function(){
-        debugger;
         //draw the ibeacons on the canvas
         //modifies the position of the beacons to better fit the screen
-        numBorder = 30;
-        factor = 120;
-        
         //beacon 1:
         $("canvas").drawPolygon({
             fillStyle: 'black',
             sides:3,
             x : numBorder, y : numBorder, //(0,0)
-            radius: 10
+            radius: radius,
         })
         
         //beacon 2: 
@@ -45,7 +83,7 @@ $(document).ready(function(){
             fillStyle: 'black',
             sides: 3,
             x : numBorder , y : numBorder + 5*factor, //(0,5) distances are multiplied by 5 and add 20
-            radius: 10
+            radius: radius,
         })
         
         //beacon 3: 
@@ -53,14 +91,13 @@ $(document).ready(function(){
             fillStyle: 'black',
             sides: 3,
             x : numBorder + 5*factor , y : numBorder, //(0,5)
-            radius: 10
+            radius: radius,
         });
-        
+            
         //Tracker:
         //getData();
         //Get data and plot it every second
         var tid = setInterval(getData,10000);
-       
     }); 
 });
 
